bbfe23294829896df332aa49bc1d8c1389f1c128
vishnu-mukati <mukativishnu1232@gmaila.com>
2024-10-24 21:32:12 +0530

    adding summary to webpage

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,39 @@
+import classes from "./AvailableMeals.module.css";
+
+const DUMMAY_MEALS = [
+    {
+        id: 'm1',
+        name: 'Sushi',
+        description: 'Finest fish and veggies',
+        price: 22.99,
+    },
+    {
+        id: 'm2',
+        name: 'Schnitzel',
+        description: 'A german specialty!',
+        price: 16.5,
+    },
+    {
+        id: 'm3',
+        name: 'Barbecue Burger',
+        description: 'American, raw, meaty',
+        price: 12.99,
+    },
+    {
+        id: 'm4',
+        name: 'Green Bowl',
+        description: 'Healthy...and green...',
+        price: 18.99,
+    },
+];
+
+const AvailableMeals = () => {
+    const MealList = DUMMAY_MEALS.map((item) =><li>{item.name}</li>)
+    return (
+        <ul className={classes.meals}>
+          {MealList}
+        </ul>
+    );
+}
+
+export default AvailableMeals;
