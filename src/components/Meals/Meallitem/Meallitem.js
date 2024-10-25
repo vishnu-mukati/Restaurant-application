446aa3a80b759e403cdbf92077cba9eb438a30d7
vishnu-mukati <mukativishnu1232@gmaila.com>
2024-10-25 11:50:36 +0530

    adding a form and a button for collecting the quantity

--- src/components/Meals/Meallitem/Meallitem.js
+++ src/components/Meals/Meallitem/Meallitem.js
@@ -1,16 +1,20 @@
 import classes from "./Meallitem.module.css";
+import MeallitemForm from "./MeallitemForm";
 
 const Meallitem = (props) => {
     const price = `$${props.price.toFixed(2)}`;
 
     return (
         <li className={classes.meal}>
+
             <div>
                 <h3>{props.name}</h3>
                 <div className={classes.description}>{props.description}</div>
                 <div className={classes.price}>{price}</div>
             </div>
-           
+           <div>
+            <MeallitemForm/>
+           </div>
         </li>
     );
 }
